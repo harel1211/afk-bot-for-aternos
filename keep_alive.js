3367be9b634bf8b812ddc057e551248c4cb0265e
harel1211 <harelhamami2011@gmail.com>
2025-06-20 09:23:17 +0300

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 2323;
+const port = 34455;
 app.get('/', (req, res) => res.send('Afk bot!'));
 
 app.listen(port, () => console.log(`Afk bot is listening to http://localhost:${port}`));
